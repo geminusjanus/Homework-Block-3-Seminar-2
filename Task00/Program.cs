e7d4a80135b7e154fd51d37b4a993ce023351322
Nikita <silvernikchuprov@gmail.com>
2022-10-11 12:13:06 +0300

    improvements

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -6,9 +6,26 @@
 
 Console.WriteLine("Введите трехзначное число ");
 int num = Convert.ToInt32(Console.ReadLine());
-while (num >= 100 && num <= 999)
+// while (num >= 100 && num <= 999)
+// {
+//     num = num / 10;
+// }
+// int secondDigit = num % 10;
+// Console.Write($"Вторая цифра числа: {secondDigit}");
+switch (num)
 {
-    num = num / 10;
+    case < 100:
+    Console.WriteLine("Число не трехзначное");
+    break;
+    case > 999:
+    Console.WriteLine("Число не трехзначное");
+    break;
+    case >= 100:
+    while (num >= 100 && num <= 999)
+    {
+        num = num / 10;
+    }
+    int secondDigit = num % 10;
+    Console.WriteLine($"Вторая цифра числа {secondDigit}");
+    break;
 }
-int secondDigit = num % 10;
-Console.Write($"Вторая цифра числа: {secondDigit}");
